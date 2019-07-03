066728b2422a5007795f48b43f02a7eea4d70b71
Sahil Siddharth <32731340+sid42mish@users.noreply.github.com>
2019-07-03 11:08:29 +0530

    Initial Commit

---
+++ face_recog.py
@@ -0,0 +1,74 @@
+import face_recognition
+import cv2
+import dlib
+import numpy as np
+import os
+
+cam = cv2.VideoCapture(0)
+
+names = os.listdir('cmpr_img')
+classes = []
+for i in names:
+    encodings = []
+    for j in range (5):
+        img = face_recognition.load_image_file("cmpr_img/"+i+"/"+str(j)+"img.jpg")
+        img_encoding = face_recognition.face_encodings(img)[0]
+        encodings.append(img_encoding)
+    classes.append(encodings)
+
+detector = dlib.get_frontal_face_detector()
+color_green = (0,255,0)
+line_width = 3
+
+
+while True:
+    ret_val, img = cam.read()
+    #rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    tracker = cv2.TrackerBoosting_create()
+    dets = detector(img)
+    for det in dets:
+        
+        # Draw rectangle around Face with color:Green and line width: 3pt
+        
+        
+        # Crop Face from the whole image frame
+        crop_img = img[det.left():det.right(), det.top():det.bottom()]
+        
+        # Create boundary box as(x, y, width, height) instead of four corner points
+        bbox = (det.left()-5, det.top()-5, det.right() - det.left()+10 ,det.bottom() - det.top()+10)
+
+        unknown_picture = face_recognition.face_locations(img)
+        flag = 0
+        try:
+            unknown_face_encoding = face_recognition.face_encodings(img,unknown_picture)[0]
+        
+            for i in range (5):
+                cv2.rectangle(img,(det.left(), det.top()), (det.right(), det.bottom()), color_green, line_width)
+                for j in range (len(names)):
+                    
+                    results1 = face_recognition.compare_faces([classes[j][i]], unknown_face_encoding, tolerance = 0.50)
+                    if results1[0] == True:
+                        #print(results1)
+                        break
+
+                        # Project image in video frame
+                if results1[0] == True:
+                    flag = 1                        
+                    cv2.putText(img, names[j], (det.left(), det.top()), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+
+                    break
+            if not flag:
+                cv2.putText(img, "none", (det.left(), det.top()), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+        except (RuntimeError, TypeError, NameError, IndexError):
+            print("no image found")
+
+        
+    cv2.imshow('LiveStream', img)
+        
+    # Add tracker for face boundaries in image
+ 
+    
+   
+    # Exit if ESC pressed    
+    k = cv2.waitKey(1) & 0xff
+    if k == 27 : break
---
+++ face_setup.py
@@ -0,0 +1,76 @@
+import face_recognition
+import cv2
+import dlib
+import numpy
+import time
+import os
+from matplotlib import pyplot as plt
+
+cam = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('haar-cascade-files-master\\haarcascade_smile.xml')
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+nmbr=0
+
+name = input("enter name: ")
+
+newpath = r'cmpr_img/'+name 
+
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+# loop runs if capturing has been initialized. 
+for i in range(50): 
+
+        ret, img = cam.read() 
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+
+        faces = face_cascade.detectMultiScale(gray, 1.3, 1)
+        pic_id = ""
+
+        for (x,y,w,h) in faces: 
+                cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2) 
+                roi_gray = gray[y:y+h, x:x+w] 
+                roi_color = img[y:y+h, x:x+w] 
+
+                # Detects eyes of different sizes in the input image 
+                eyes = eye_cascade.detectMultiScale(roi_gray)
+                smile = smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.9, minNeighbors=4, minSize=(10, 10),flags = cv2.CASCADE_SCALE_IMAGE)
+                
+                #To draw a rectangle in eyes
+                if len(eyes) != 0:
+                        
+                        crop_img=img[y:y+h, x:x+w]
+                        cv2.imwrite('cmpr_img\\'+name+'\\'+str(nmbr)+'img.jpg',crop_img)
+                        nmbr+=1
+                        print(nmbr)
+                        #cv2.imshow('img', )
+                        #plt.show()
+                        '''unknown_face_encoding = face_recognition.face_encodings(crop_img)[0]
+                        results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
+                        if results[0] == True:
+                            print("It's a picture of me!")
+                            pic_id="SRK"
+                        else:
+                            print("It's not a picture of me!")
+                            pic_id="not SRK" '''
+
+                
+                cv2.putText(img,pic_id,(x,y), font, 1,(255,255,255),2,cv2.LINE_AA)
+        # Display an image in a window 
+        if nmbr==5:
+            break
+        cv2.imshow('img',img)
+        #time.sleep(4)
+
+        # Wait for Esc key to stop 
+        k = cv2.waitKey(30) & 0xff
+        if k == 27: 
+                break
+
+cam.release() 
+
+cv2.destroyAllWindows() 
