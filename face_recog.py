4989da699a17972c3883f32afc98ad9a607c1fae
Snow <17dcs013@lnmiit.ac.in>
2019-07-04 16:09:18 +0530

    added comments

--- face_recog.py
+++ face_recog.py
@@ -4,71 +4,91 @@ import dlib
 import numpy as np
 import os
 
-cam = cv2.VideoCapture(0)
+ ####################################################################################
+##	A.face_recog : Attendance using Face Recognition Project
+##		1. Detecting face
+##		2. Classifying faces by comparing unknown face with known faces						
+##		3. Marking attendance for checked classes
+ ####################################################################################
 
-names = os.listdir('cmpr_img')
+cam = cv2.VideoCapture(0)       # setting up camera
+
+names = os.listdir('cmpr_img')  # getting classes from directory
 classes = []
+
+nmbr_of_img = 5                 # number of images in a class
+min_imgs = 2                    # minimum hits required
+
 for i in names:
     encodings = []
-    for j in range (5):
+    for j in range (nmbr_of_img):
+        # loading images from dir
         img = face_recognition.load_image_file("cmpr_img/"+i+"/"+str(j)+"img.jpg")
+        # 128-dimension face encoding for each face
         img_encoding = face_recognition.face_encodings(img)[0]
-        encodings.append(img_encoding)
-    classes.append(encodings)
+        encodings.append(img_encoding)  # appending encoded images in list 'encodings'
+    classes.append(encodings)           # appending encodings in 2-D list 'classes'
+    
+attendance =  {key: 0 for key in names} # initialising attendance list
+print(attendance)
+detector = dlib.get_frontal_face_detector() # detecting frontal face
+color_green = (0,255,0)                     # color of box
+line_width = 3                              # width of box
 
-detector = dlib.get_frontal_face_detector()
-color_green = (0,255,0)
-line_width = 3
-
-
+# during live video feed
 while True:
-    ret_val, img = cam.read()
-    #rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #tracker = cv2.TrackerBoosting_create()
-    dets = detector(img)
-    for det in dets:
-        
-        # Draw rectangle around Face with color:Green and line width: 3pt
-        
-        
+    ret_val, img = cam.read()               # getting frame from live feed
+    dets = detector(img)                    # detecting frontal faces in frame
+
+    for det in dets:     
         # Crop Face from the whole image frame
         crop_img = img[det.left():det.right(), det.top():det.bottom()]
         
         # Create boundary box as(x, y, width, height) instead of four corner points
         bbox = (det.left()-5, det.top()-5, det.right() - det.left()+10 ,det.bottom() - det.top()+10)
 
+        # Detecting face in frame
         unknown_picture = face_recognition.face_locations(img)
-        flag = 0
+        flag = 0                            # initialising flag
+        count = [0]*len(names)              # initialising count
         try:
             unknown_face_encoding = face_recognition.face_encodings(img,unknown_picture)[0]
         
-            for i in range (5):
+            for i in range (nmbr_of_img):
+                # Draw rectangle around Face with color:Green and line width: 3pt
                 cv2.rectangle(img,(det.left(), det.top()), (det.right(), det.bottom()), color_green, line_width)
                 for j in range (len(names)):
-                    
-                    results1 = face_recognition.compare_faces([classes[j][i]], unknown_face_encoding, tolerance = 0.50)
-                    if results1[0] == True:
-                        #print(results1)
-                        break
+                    # comparing unknown faces with known classes
+                    results1 = face_recognition.compare_faces([classes[j][i]], unknown_face_encoding, tolerance = 0.45)
+                    if results1[0] == True:         # checking matches
+                    	count[j] += 1               # increment count of that class
+    
+                    if count[j] > min_imgs:         # checking if count of that class exceeds min images
+                    	break
 
-                        # Project image in video frame
-                if results1[0] == True:
-                    flag = 1                        
+                # Project image in video frame
+                if results1[0] == True and count[j] > min_imgs: # checking if matched and min images condition
+                    flag = 1                        # setting flag to 1
+                    attendance[names[j]] = 1        # marking present to that class
+                    # putting name of that class beside box
                     cv2.putText(img, names[j], (det.left(), det.top()), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
-
                     break
+
             if not flag:
+                # setting that image to 'none' class
                 cv2.putText(img, "none", (det.left(), det.top()), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
         except (RuntimeError, TypeError, NameError, IndexError):
             print("no image found")
 
         
-    cv2.imshow('LiveStream', img)
-        
-    # Add tracker for face boundaries in image
+    cv2.imshow('LiveStream', img)                   # displaying live stream on screen
  
     
    
     # Exit if ESC pressed    
     k = cv2.waitKey(1) & 0xff
-    if k == 27 : break
+    if k == 97:
+        print(attendance)                           # printing attendance if 'a' is pressed
+    if k == 27 : 
+    	print(attendance)                           # printing final attendance when ESC is pressed
+    	break
--- face_setup.py
+++ face_setup.py
@@ -6,70 +6,65 @@ import time
 import os
 from matplotlib import pyplot as plt
 
-cam = cv2.VideoCapture(0)
+ ####################################################################################
+##	A.face_setup : Attendance using Face Recognition Project
+##		1. Detecting face
+##		2. Cropping image							
+##		3. Saving it in directory							
+ ####################################################################################
 
+
+cam = cv2.VideoCapture(0)				# Setting up camera
+
+# Loading cascade files of frontalface, eyes, smile
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-nmbr=0
 
-name = input("enter name: ")
+font = cv2.FONT_HERSHEY_SIMPLEX				# setting font
+nmbr=0							# intialising number of images to 0
+name = input("enter name: ")				# getting input name
+newpath = r'cmpr_img/'+name 				# intialising location of new directory
 
-newpath = r'cmpr_img/'+name 
-
-if not os.path.exists(newpath):
+if not os.path.exists(newpath):				# make new path if path doesn't exist
     os.makedirs(newpath)
 # loop runs if capturing has been initialized. 
 for i in range(50): 
 
-        ret, img = cam.read() 
-
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-
-        faces = face_cascade.detectMultiScale(gray, 1.3, 1)
-        pic_id = ""
+        ret, img = cam.read() 				# starting live video
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # converting img to grayscale
+        faces = face_cascade.detectMultiScale(gray, 1.3, 1) # detecting frontal face
 
         for (x,y,w,h) in faces: 
                 cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2) 
                 roi_gray = gray[y:y+h, x:x+w] 
                 roi_color = img[y:y+h, x:x+w] 
 
-                # Detects eyes of different sizes in the input image 
+                # Detects eyes and smile of different sizes in the input image 
                 eyes = eye_cascade.detectMultiScale(roi_gray)
                 smile = smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.9, minNeighbors=4, minSize=(10, 10),flags = cv2.CASCADE_SCALE_IMAGE)
                 
                 #To draw a rectangle in eyes
                 if len(eyes) != 0:
+                    crop_img=img[y:y+h, x:x+w] 		# cropping image to the size of face
+                    try:
+                       	# the 128-dimension face encoding for each face
+                       	encoding = face_recognition.face_encodings(crop_img)[0]		
+                    except (RuntimeError, TypeError, NameError, IndexError):
+                        print('Incorrect format')
+                        break
+                    # saving cropped image to the directory
+                    cv2.imwrite('cmpr_img\\'+name+'\\'+str(nmbr)+'img.jpg',crop_img)
+                    nmbr+=1				# incrementing the number of images in the dir
+                    print(nmbr)
                         
-                        crop_img=img[y:y+h, x:x+w]
-                        cv2.imwrite('cmpr_img\\'+name+'\\'+str(nmbr)+'img.jpg',crop_img)
-                        nmbr+=1
-                        print(nmbr)
-                        #cv2.imshow('img', )
-                        #plt.show()
-                        '''unknown_face_encoding = face_recognition.face_encodings(crop_img)[0]
-                        results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
-                        if results[0] == True:
-                            print("It's a picture of me!")
-                            pic_id="SRK"
-                        else:
-                            print("It's not a picture of me!")
-                            pic_id="not SRK" '''
-
-                
-                cv2.putText(img,pic_id,(x,y), font, 1,(255,255,255),2,cv2.LINE_AA)
-        # Display an image in a window 
-        if nmbr==5:
+        if nmbr>4: 					# break before number of images in the dir exceeds 5
             break
         cv2.imshow('img',img)
-        #time.sleep(4)
-
-        # Wait for Esc key to stop 
-        k = cv2.waitKey(30) & 0xff
+        k = cv2.waitKey(30) & 0xff			# wait for Esc key to stop 
         if k == 27: 
-                break
+        	break
 
 cam.release() 
 
